9c380ca3e994b52848a14b05191c3f9001298e15
ipshenyc <inna_pshenychna_tp@bmc.com>
2017-07-05 22:06:19 +0300

    fix Task6 and Task8 according to comments

--- src/test/java/Task6AddNewItem.java
+++ src/test/java/Task6AddNewItem.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.junit.Assert.*;
 import java.io.File;
@@ -26,7 +27,8 @@ public class Task6AddNewItem {
     private String logIn = "admin";
     public WebDriver driver;
     public WebDriverWait wait;
-    public String  itemName = "Test Item 2";
+    public String  itemName = "IronMan";
+    public String baseURL="http://localhost:9000/litecart/admin/"; //litecart version 2.0.1
 
 
     @Before
@@ -37,7 +39,7 @@ public class Task6AddNewItem {
         wait = new WebDriverWait(driver, 10);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         System.out.println("Logging in to AC");
-        driver.get("http://localhost:9000/litecart/admin/");
+        driver.get(baseURL);
         driver.findElement(USER_NAME_INPUT_FIELD).sendKeys(logIn);
         driver.findElement(PASSWORD_INPUT_FIELD).sendKeys(logIn);
         driver.findElement(LOGIN_BUTTON).click();
@@ -57,37 +59,40 @@ public class Task6AddNewItem {
     @Test
     public void addNewItemAC() {
         System.out.println("Adding new Item in AC");
-        wait.until(elementToBeClickable(By.xpath("//*[@href='http://localhost:9000/litecart/admin/?app=catalog&doc=catalog']")));
-        driver.findElement(By.xpath("//*[@href='http://localhost:9000/litecart/admin/?app=catalog&doc=catalog']")).click();
-        driver.findElement(By.xpath("//*[@href='http://localhost:9000/litecart/admin/?category_id=0&app=catalog&doc=edit_product']")).click();
+
+        wait.until(elementToBeClickable(By.xpath("//*[contains(@href,'/?app=catalog&doc=catalog')]")));
+        driver.findElement(By.xpath("//*[contains(@href,'/?app=catalog&doc=catalog')]")).click();
+        driver.findElement(By.xpath("//*[contains(@href,'/?category_id=0&app=catalog&doc=edit_product')]")).click();
 
         wait.until(visibilityOfElementLocated(By.cssSelector("#main>h1")));
-
-         ClassLoader classLoader = getClass().getClassLoader();
+        assertTrue(isElementPresent(By.name("name[en]")));
+        ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("ironman.jpg").getFile());
         driver.findElement(By.name("new_images[]")).sendKeys(file.getAbsolutePath());
         System.out.println("Fill in general");
-        driver.findElement(By.xpath("html/body/div[2]/main/form/div/div/div[1]/div/div[1]/div[1]/div/div/label[1]")).click();
-        driver.findElement(By.xpath("html/body/div[2]/main/form/div/div/div[1]/div/div[2]/div[1]/input")).sendKeys("0000011");
-        driver.findElement(By.xpath("html/body/div[2]/main/form/div/div/div[1]/div/div[2]/div[2]/div/input")).sendKeys(itemName);
-        driver.findElement(By.xpath("html/body/div[2]/main/form/div/div/div[1]/div/div[2]/div[4]/div/div[1]/input")).clear();
-        driver.findElement(By.xpath("html/body/div[2]/main/form/div/div/div[1]/div/div[2]/div[4]/div/div[1]/input")).sendKeys("1");
-        driver.findElement(By.xpath("html/body/div[2]/main/form/div/div/div[1]/div/div[2]/div[5]/div/input")).clear();
-        driver.findElement(By.xpath("html/body/div[2]/main/form/div/div/div[1]/div/div[2]/div[5]/div/input")).sendKeys("1");
-        driver.findElement(By.xpath("html/body/div[2]/main/form/div/div/div[1]/div/div[2]/div[6]/div/input[1]")).sendKeys("10");
-        driver.findElement(By.xpath("html/body/div[2]/main/form/div/div/div[1]/div/div[2]/div[6]/div/input[2]")).sendKeys("20");
-        driver.findElement(By.xpath("html/body/div[2]/main/form/div/div/div[1]/div/div[2]/div[6]/div/input[3]")).sendKeys("30");
-        driver.findElement(By.xpath("html/body/div[2]/main/form/div/div/div[1]/div/div[1]/div[4]/div/div/div[3]")).click();
-        driver.findElement(By.xpath("html/body/div[2]/main/form/div/div/div[1]/div/div[1]/div[5]/input")).sendKeys("2017-06-06");
-       driver.findElement(By.xpath("html/body/div[2]/main/form/div/div/div[1]/div/div[1]/div[6]/input")).sendKeys("2017-11-08");
+        driver.findElement(By.xpath("//*[@class='tab-pane active']//*[@class='btn btn-default']")).click();
+        driver.findElement(By.name("code")).sendKeys("0000011");
+        driver.findElement(By.name("name[en]")).sendKeys(itemName);
+        driver.findElement(By.name("quantity")).clear();
+        driver.findElement(By.name("quantity")).sendKeys("1");
+        driver.findElement(By.name("weight")).clear();
+        driver.findElement(By.name("weight")).sendKeys("1");
+        driver.findElement(By.name("dim_x")).sendKeys("10");
+        driver.findElement(By.name("dim_y")).sendKeys("20");
+        driver.findElement(By.name("dim_z")).sendKeys("30");
+        driver.findElement(By.xpath("//*[contains(.,'Gender')]//*[@class='checkbox'][2]")).click();
+        driver.findElement(By.name("date_valid_from")).sendKeys("2017-06-06");
+        driver.findElement(By.name("date_valid_to")).sendKeys("2017-11-08");
+        Select dropdown = new Select(driver.findElement(By.name("sold_out_status_id")));
+        dropdown.selectByVisibleText("Sold out");
 
         System.out.println("Fill in price and save");
-        driver.findElement(By.xpath("html/body/div[2]/main/form/div/ul/li[3]/a")).click();
-        driver.findElement(By.xpath("html/body/div[2]/main/form/div/div/div[3]/div[1]/div/div[1]/div/input")).sendKeys("10");
-        driver.findElement(By.xpath("html/body/div[2]/main/form/div/div/div[3]/div[1]/table/tbody/tr[1]/td[1]/div/input")).sendKeys("20");
+        driver.findElement(By.xpath("//*[@href='#tab-prices']")).click();
+        driver.findElement(By.name("purchase_price")).sendKeys("10");
+        driver.findElement(By.name("prices[USD]")).sendKeys("20");
+        driver.findElement(By.name("save")).click();
 
         System.out.println("Verify if item is present in list on AC");
-        driver.findElement(By.xpath("html/body/div[2]/main/form/p/button[1]")).click();
         wait.until(visibilityOfElementLocated(By.cssSelector(".table.table-striped")));
         String createdItemLocator = String.format("//a[contains(.,'%s')]", itemName);
         assertTrue(isElementPresent(By.xpath(createdItemLocator)));
@@ -95,7 +100,7 @@ public class Task6AddNewItem {
     }
 
 
-    public boolean isElementPresent(By locator) {
+    private boolean isElementPresent(By locator) {
 
         return driver.findElements(locator).size() > 0;
 
--- src/test/java/Task8.java
+++ src/test/java/Task8.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -13,7 +14,6 @@ import static org.junit.Assert.*;
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -27,9 +27,6 @@ public class Task8 {
     private String logIn = "admin";
     public WebDriver driver;
     public WebDriverWait wait;
-    public List<String> allLinksLocators = Arrays.asList("//*[@href='http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2']","//*[@href='http://en.wikipedia.org/wiki/ISO_3166-1_alpha-3']","//*[@href='https://en.wikipedia.org/wiki/Regular_expression']"
-            ,"//*[@href='http://www.addressdoctor.com/en/countries-data/address-formats.html']","//*[@href='https://en.wikipedia.org/wiki/Regular_expression']","//*[@href='https://en.wikipedia.org/wiki/List_of_countries_and_capitals_with_currency_and_language']"
-            ,"//*[@href='https://en.wikipedia.org/wiki/List_of_country_calling_codes']");
 
 
     @Before
@@ -50,7 +47,7 @@ public class Task8 {
         driver.findElement(USER_NAME_INPUT_FIELD).sendKeys(logIn);
         driver.findElement(PASSWORD_INPUT_FIELD).sendKeys(logIn);
         driver.findElement(LOGIN_BUTTON).click();
-        wait.until(elementToBeClickable(By.xpath("//*[@href='http://localhost:9000/litecart/admin/?app=catalog&doc=catalog']")));
+        wait.until(elementToBeClickable(By.xpath("//*[contains(@href,'/?app=catalog&doc=catalog')]")));
 
 
     }
@@ -69,16 +66,25 @@ public class Task8 {
         System.out.println("Checking if links opened in new window");
 
         driver.get("http://localhost:9000/litecart/admin/?app=countries&doc=countries");
-        driver.findElement(By.xpath("//*[@href='http://localhost:9000/litecart/admin/?app=countries&doc=edit_country']")).click();
+        driver.findElement(By.xpath("//*[contains(@href,'/?app=countries&doc=edit_country')]")).click();
         String originalHandle = driver.getWindowHandle();
         Set<String> existWs = driver.getWindowHandles();
         System.out.println(originalHandle);
 
-        for(String link: allLinksLocators){
+        List<WebElement>  linksList = driver.findElements(By.cssSelector(".fa.fa-external-link"));
+        for(WebElement link: linksList){
             verifyThatWindowsOpened (link, existWs);
             driver.switchTo().window(originalHandle);
         }
+    }
 
+    public void verifyThatWindowsOpened (WebElement link, Set<String> existingWindows){
+        link.click();
+        String newW = wait.until(anyWindowOtherThan(existingWindows));
+        driver.switchTo().window(newW);
+        System.out.println(newW);
+        assertFalse(existingWindows.contains(newW));
+        driver.close();
     }
 
     public ExpectedCondition<String> anyWindowOtherThan(Set<String> windows) {
@@ -101,14 +107,5 @@ public class Task8 {
     }
 
 
-    public void verifyThatWindowsOpened (String link, Set<String> existingWindows){
-        driver.findElement(By.xpath(link)).click();
-        String newW = wait.until(anyWindowOtherThan(existingWindows));
-        driver.switchTo().window(newW);
-        System.out.println(newW);
-        assertFalse(existingWindows.contains(newW));
-        driver.close();
-
-    }
 
 }
